b61c039a44e81277866df5a037d6e539f71a8cd2
Kyle McVeigh <kylemcv@gmail.com>
2025-04-18 15:59:20 -0400

    Update env variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,8 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/my_auth_stream_access_endpoint', (req, res) => {
-    console.log(req)
-    console.log(body)
+    console.log(req.body)
     // const { body: {token, amount, merchant}} = req;
   	// Custom Approve/Decline Logic ...
     // ... ex. decline if amount is > $500 ...
@@ -16,6 +16,7 @@ app.post('/my_auth_stream_access_endpoint', (req, res) => {
     // ... ex. decline if card was created within the last day ...
 
     const result = 'DECLINED'; // or 'DECLINED', 'UNAUTHORIZED_MERCHANT', etc.
+    const token = process.env.LITHIC_API_KEY
     return res.json({ result, token })
 });
 
